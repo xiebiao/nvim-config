fe30d5cf4b80e27fb38f9bff5037d05f982c1d81
xiebiao <joyrap@gmail.com>
2023-11-07 11:00:00 +0800

    窗口聚焦时，自动调整窗口大小

--- lua/biaoxie/core/keymaps.lua
+++ lua/biaoxie/core/keymaps.lua
@@ -4,7 +4,9 @@ local keymap = vim.api.nvim_set_keymap
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
-keymap('i','jk','<ESC>',opts) -- 在insert模式下同时按下jk键进入Normal模式
+-- TODO 插入模式下，输入`j`会有延迟，建议不要设置
+--keymap('i','jk','<ESC>',opts) -- 在insert模式下同时按下jk键进入Normal模式
+
 keymap('n','<leader>w',':w!<ESC>',opts) --normal模式下快捷保存文件 
 
 -- normal,visual模式下向上/向下移动内容
@@ -12,11 +14,24 @@ keymap('n','<S-Down>','mz:m+<cr>`z',opts)
 keymap('n','<S-Up>','mz:m-2<cr>`z',opts)
 keymap('v','<S-Down>',":m'>+<cr>`<my`>mzgv`yo`z",opts)
 keymap('v','<S-Up>',":m'<-2<cr>`>my`<mzgv`yo`z",opts)
--- 窗口相关
+
+-- 创建窗口
+
+-- 窗口切换
 keymap('n','<C-j>','<C-w>j',opts)
 keymap('n','<C-k>','<C-w>k',opts)
 keymap('n','<C-h>','<C-w>h',opts)
 keymap('n','<C-l>','<C-w>l',opts)
+
+-- 垂直窗口聚焦时，自动调整窗口宽度为85，在多窗口下非常有用
+vim.api.nvim_create_autocmd(
+  {"CursorMoved"},
+  {
+    callback = function()
+      vim.fn.execute(":vert res 85")
+    end
+  } 
+)
 
 -- nvim-tree
 keymap('n','<leader>d',':NvimTreeToggle <CR>',opts)
